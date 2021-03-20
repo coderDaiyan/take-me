186a65152adc267fce54c9d498ac49c755d1e09c
coderDaiyan <daiyanabdallah@gmail.com>
2021-03-20 23:46:33 +0600

    latest code pus

--- src/Components/Destination/Destination.js
+++ src/Components/Destination/Destination.js
@@ -82,14 +82,14 @@ const PickUpPoint = () => {
       <div className="map">
         <div className="map-img">
           <iframe
-            title="Google Maps"
+            title="Google Map"
+            src="https://www.google.com/maps/d/embed?mid=1cKzlQwRa_rKNPDi9Vz6D24WuNCI"
             width="100%"
             height="600"
             frameborder="0"
             scrolling="no"
             marginheight="0"
             marginwidth="0"
-            src="https://maps.google.com/maps?width=50025&amp;height=600&amp;hl=en&amp;q=1%20Grafton%20Street,%20Dublin,%20Ireland+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
           ></iframe>
         </div>
       </div>
