db33a12c1fbbedce754c144e2c0203f26e254958
coderDaiyan <daiyanabdallah@gmail.com>
2021-03-21 18:50:36 +0600

    respnsive part-1 done

--- src/Components/Destination/Destination.js
+++ src/Components/Destination/Destination.js
@@ -45,86 +45,86 @@ const PickUpPoint = () => {
   };
   const { vehicle, vehicleIcon, capacity, price } = searchResult;
   return (
-    <div className="parent" style={{ marginTop: "7rem" }}>
-      <div style={{ height: result && "460px" }} className="pick-up">
-        {!result && (
-          <form onSubmit={handleSearch} action="">
-            <h5>Pick From</h5>
-            <input
-              className="form-control mt-3 mb-3"
-              name="from"
-              onBlur={handleBlur}
-              type="text"
-              required
-            />
-            <h5>Pick To</h5>
-            <input
-              className="form-control mt-3 mb-3"
-              name="to"
-              type="text"
-              onBlur={handleBlur}
-              required
-            />
-            <button className="w-100 btn btn-success" type="submit">
-              Search
-            </button>
-          </form>
-        )}
-        {result && (
-          <div className="orange-destination-div">
-            <h4>From: {destination?.from}</h4>
-            <br />
-            <h4>To: {destination?.to}</h4>
-          </div>
-        )}
-        {result && (
-          <div className="search-results">
-            <img
-              className="vehicle-icon"
-              src={searchResult.vehicleIcon && vehicleIcon}
-              alt=""
-            />
-            <h4>{searchResult.vehicle && vehicle}</h4>
-            <img className="people-icon" src={People} alt="" />
-            <h4>{searchResult && capacity}</h4>
-            <div className="price">
-              <h4>${searchResult && price}</h4>
+    <div className="container">
+      <div className="parent row g-4" style={{ marginTop: "7rem" }}>
+        <div style={{ height: result && "460px" }} className="pick-up col-lg-6">
+          {!result && (
+            <form onSubmit={handleSearch} action="">
+              <h5>Pick From</h5>
+              <input
+                className="form-control mt-3 mb-3"
+                name="from"
+                onBlur={handleBlur}
+                type="text"
+                required
+              />
+              <h5>Pick To</h5>
+              <input
+                className="form-control mt-3 mb-3"
+                name="to"
+                type="text"
+                onBlur={handleBlur}
+                required
+              />
+              <button className="w-100 btn btn-success" type="submit">
+                Search
+              </button>
+            </form>
+          )}
+          {result && (
+            <div className="orange-destination-div">
+              <h4>From: {destination?.from}</h4>
+              <br />
+              <h4>To: {destination?.to}</h4>
             </div>
-          </div>
-        )}
-        {result && (
-          <div className="search-results">
-            <img
-              className="vehicle-icon"
-              src={searchResult.vehicleIcon && vehicleIcon}
-              alt=""
-            />
-            <h4>{searchResult.vehicle && vehicle}</h4>
-            <img className="people-icon" src={People} alt="" />
-            <h4>{searchResult && capacity}</h4>
-            <div className="price">
-              <h4>${searchResult && price}</h4>
+          )}
+          {result && (
+            <div className="search-results">
+              <img
+                className="vehicle-icon"
+                src={searchResult.vehicleIcon && vehicleIcon}
+                alt=""
+              />
+              <h4>{searchResult.vehicle && vehicle}</h4>
+              <img className="people-icon" src={People} alt="" />
+              <h4>{searchResult && capacity}</h4>
+              <div className="price">
+                <h4>${searchResult && price}</h4>
+              </div>
             </div>
-          </div>
-        )}
-        {result && (
-          <div className="search-results">
-            <img
-              className="vehicle-icon"
-              src={searchResult.vehicleIcon && vehicleIcon}
-              alt=""
-            />
-            <h4>{searchResult.vehicle && vehicle}</h4>
-            <img className="people-icon" src={People} alt="" />
-            <h4>{searchResult && capacity}</h4>
-            <div className="price">
-              <h4>${searchResult && price}</h4>
+          )}
+          {result && (
+            <div className="search-results">
+              <img
+                className="vehicle-icon"
+                src={searchResult.vehicleIcon && vehicleIcon}
+                alt=""
+              />
+              <h4>{searchResult.vehicle && vehicle}</h4>
+              <img className="people-icon" src={People} alt="" />
+              <h4>{searchResult && capacity}</h4>
+              <div className="price">
+                <h4>${searchResult && price}</h4>
+              </div>
             </div>
-          </div>
-        )}
-      </div>
-      <div className="map">
-        <div className="map-img">
+          )}
+          {result && (
+            <div className="search-results">
+              <img
+                className="vehicle-icon"
+                src={searchResult.vehicleIcon && vehicleIcon}
+                alt=""
+              />
+              <h4>{searchResult.vehicle && vehicle}</h4>
+              <img className="people-icon" src={People} alt="" />
+              <h4>{searchResult && capacity}</h4>
+              <div className="price">
+                <h4>${searchResult && price}</h4>
+              </div>
+            </div>
+          )}
+        </div>
+        <div className="map col-lg-6">
           <ReactMapGL
             mapboxApiAccessToken={
               "pk.eyJ1IjoiZGV2ZWxvcGVyZGFpeWFuIiwiYSI6ImNrbWl0bDdwejBrcjgybm52YWg5bnhxOHUifQ.geGOlcnfET9ER3Bv5TNmaw"
